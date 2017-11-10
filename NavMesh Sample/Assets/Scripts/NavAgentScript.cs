278a901d620973b81d4b021fc2f75a78d96d5463
Josiah Maius S. Ebia <josiah_ebia@yahoo.com>
2017-11-10 14:20:21 +0800

    Sample navmesh project where agent goes to target on run

---
+++ NavMesh Sample/Assets/Scripts/NavAgentScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavAgentScript : MonoBehaviour {
+
+    public GameObject targetObject;
+    public GameObject wall;
+
+    private NavMeshAgent agent;
+
+    private float startTime;
+
+    private bool isFloating;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+        agent = this.GetComponent<NavMeshAgent>();
+
+        agent.SetDestination(targetObject.transform.position);
+
+        startTime = Time.deltaTime;
+
+        isFloating = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+    }
+}
+
+
+
---
+++ NavMesh Sample/Assets/Scripts/WallController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallController : MonoBehaviour {
+
+    private bool isFloating;
+
+	// Use this for initialization
+	void Start () {
+        isFloating = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space") && !isFloating)
+        {
+            isFloating = !isFloating;
+            this.transform.Translate(0, 3, 0);
+        }
+    }
+}
